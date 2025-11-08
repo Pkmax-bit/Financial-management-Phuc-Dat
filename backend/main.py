c2391f4bae38f929f7bb3e15a800a666d5e40993
pkmax-bit <2100010779@nttu.edu.vn>
2025-11-08 13:21:52 +0700

    đổi cổng window->linux

--- backend/main.py
+++ backend/main.py
@@ -111,7 +111,7 @@ app.include_router(material_adjustment_rules.router, prefix="/api/material-adjus
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
-        host="0.0.0.0",
+        host="10.0.2.15",
         port=8000,
         reload=True,
         log_level="info"
